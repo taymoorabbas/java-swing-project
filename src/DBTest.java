621ed8f6dc840d144dcb2cc680b0aa976559a1c0
taymoorabbas <www.taymoor@gmail.com>
2019-12-22 22:26:18 +0500

    added connectivity to mysql database

---
+++ src/DBTest.java
@@ -0,0 +1,18 @@
+/*
+Created by: Taymoor Ghazanfar
+R.no: 3625-BSSE-F17-C
+Date: 22-Dec-19
+Time: 10:12 PM
+Lau ji Ghauri aya fir
+*/
+
+import model.Database;
+
+public class DBTest {
+
+    public static void main(String[] args) {
+
+        Database database = new Database();
+        database.connect();
+    }
+}
--- src/model/Database.java
+++ src/model/Database.java
@@ -9,15 +9,65 @@ Lau ji Ghauri aya fir
 package model;
 
 import java.io.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Database {
 
     private List<Person> people;
+    private Connection connection;
 
     public Database(){
 
         this.people = new LinkedList<>();
+    }
+
+    public void connect(){
+
+        if(connection != null){
+
+            return;
+        }
+        try {
+            System.out.println("Requesting for connection. . .");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = "jdbc:mysql://localhost:3306/database_1";
+            connection = DriverManager.getConnection(url, "root", "471582");
+            System.out.println("Connection established to: " + connection);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void disconnect(){
+
+        try {
+            if(this.connection != null){
+                this.connection.close();
+                System.out.println("Database disconnected");
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addPerson(Person person){
